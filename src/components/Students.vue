dbcebea0856635f4a0fdd3de9af0237a510b86f2
Sherlock <sherlock@gmail.com>
2018-06-05 16:18:43 +0630

    Displayed students in Students.vue

--- src/components/Students.vue
+++ src/components/Students.vue
@@ -1,5 +1,12 @@
 <template>
-    
+    <ul>
+        <li v-for="student in students" v-bind:key="student.id">
+            {{ student.name }}
+            <i>{{ student.char.length }} characters he has.</i>
+            <button>Team A</button>
+            <button>Team B</button>
+        </li>
+    </ul>
 </template>
 
 <script>
